77f7448d9bce39726b306008f7fd4e82fa551e24
sean.wedig@gmail.com <sean.wedig@gmail.com>
2024-02-04 14:35:33 -0500

    feat: indexing with re-combined code from PDF investigation

---
+++ indexing/indexing/index_5e_srd.py
@@ -0,0 +1,24 @@
+import argparse
+from parsing.srd_5e import SrdParser
+
+
+def main(srd_pdf, output, env):
+    parser = SrdParser(srd_pdf)
+    chunks = parser.chunk_srd_pdf()
+    print(chunks[100])
+
+
+
+if __name__ == '__main__':
+    # take arguments for source SRD PDF, the output vector store, and a .env file
+    parser = argparse.ArgumentParser(description='Index the 5e SRD')
+    parser.add_argument('--srd-pdf', type=str, required=True, help='The source PDF file of the 5e SRD (the CC version), avaialble here: https://dnd.wizards.com/resources/systems-reference-document')
+    parser.add_argument('--output', type=str, required=True, help='The output directory for the SQLite vector store from indexing')
+    parser.add_argument('--env', type=str, default='.env', help='The .env file for API keys and other sensitive information')
+
+    args = parser.parse_args()
+    srd_pdf = args.srd_pdf
+    output = args.output
+    env = args.env
+
+    main(srd_pdf, output, env)
---
+++ indexing/indexing/parsing/srd_5e.py
@@ -0,0 +1,113 @@
+import fitz
+import re
+
+class SrdParser():
+    CHAPTER = { 'font': 'GillSans-SemiBold', 'size': 25.920000076293945, 'color': 9647668 } # ('Races'): 'GillSans-SemiBold', size ~25.920000076293945, color 9647668
+    SECTION = { 'font': 'GillSans-SemiBold', 'size': 18, 'color': 9647668 } # ('Dwarf'): 'GillSans-SemiBold', size 18, color 9647668
+    SUBSECTION = { 'font': 'GillSans-SemiBold', 'size': 13.920000076293945, 'color': 9647668 } # ('Racial Traits'): 'GillSans-SemiBold', size ~13.920000076293945, color 9647668
+    SUBSUBSECTION = { 'font': 'GillSans-SemiBold', 'size': 12, 'color': 9647668 } # ('Ability Score Increase'): 'GillSans-SemiBold', size 12, color 9647668
+
+    # TODO: term & definition - e.g., Adventuring Gear with the bold, italic lead in to the paragraph
+    # TODO: table header / capturing tables
+    # TODO: sidebars like "Self-Sufficiency" on page 73E
+    # TODO: handle two back-to-back sections, like "Using Ablility" followed by "Scores" on 76
+    # TODO: monster headers for individual monster stat blocks
+
+    def __init__(self, pdf_path: str) -> None:
+        self.pdf_path = pdf_path
+        self.doc = fitz.open(pdf_path)
+
+
+    def chunk_srd_pdf(self):
+        wayfinding = { 'chapter': 'Legal Information', 'section': None, 'subsection': None, 'subsubsection': None, 'starting_page': None}
+        chunk_content = ""
+        doc_chunks = []
+
+        for i, page in enumerate(self.doc):
+            page_num = i+1
+
+            d = page.get_text('dict')
+            blocks = d['blocks']
+            for block in blocks:
+                if "lines" in block.keys():
+                    spans = block['lines']
+                    for span in spans:
+                        data = span['spans']
+                        for lines in data:
+                            found_heading = False
+                            trimmed_text = self.__clean_text_lines(lines['text'].strip())
+                            # clone last heading
+                            last_heading = wayfinding.copy()
+
+                            # each new heading resets the wayfinding for the lower levels
+                            if self.__is_heading_block(lines, self.CHAPTER):
+                                wayfinding['chapter'] = trimmed_text
+                                wayfinding['section'] = None
+                                wayfinding['subsection'] = None
+                                wayfinding['subsubsection'] = None
+                                found_heading = True
+
+                            if self.__is_heading_block(lines, self.SECTION):
+                                wayfinding['section'] = trimmed_text
+                                wayfinding['subsection'] = None
+                                wayfinding['subsubsection'] = None
+                                found_heading = True
+
+                            if self.__is_heading_block(lines, self.SUBSECTION):
+                                wayfinding['subsection'] = trimmed_text
+                                wayfinding['subsubsection'] = None
+                                found_heading = True
+
+                            if self.__is_heading_block(lines, self.SUBSUBSECTION):
+                                wayfinding['subsubsection'] = trimmed_text
+                                found_heading = True
+
+                            if not found_heading:
+                                chunk_content += trimmed_text + " "
+                            else:
+                                # end the content of the previous chunk, if there was any
+                                if chunk_content != "":
+                                    previous_chunk = {
+                                        'location': last_heading,
+                                        'content': chunk_content
+                                    }
+                                    doc_chunks.append(previous_chunk)
+                                chunk_content = ""
+                                wayfinding['starting_page'] = page_num
+
+        # finish the last chunk
+        previous_chunk = {
+            'location': wayfinding,
+            'content': chunk_content
+        }
+        doc_chunks.append(previous_chunk)
+        
+        return doc_chunks
+
+
+    def __strip_boilerplate(self, text: str) -> str:
+        text = text
+        page_boilerplate = 'System\xa0Reference\xa0Document\xa05.1\xa0\n\xa0 \d+\xa0\n'
+        text = re.sub(page_boilerplate, '', text)
+        text = re.sub('System\s+Reference\s+Document\s+5.1\s+\d+\s+', '', text)
+        
+        error_note = 'If you note any errors in this document, please let us know by \nemailing askdnd@wizards.com. \n'
+        if error_note in text:
+            text = text.replace(error_note, '')
+    
+        return text
+
+
+    def __clean_text_lines(self, text: str) -> str:
+        text = text.replace('\t\r \xa0', ' ')
+        text = text.replace('\n', ' ')
+        text = text.replace('\xa0', ' ')
+        text = text.replace('-\xad‐‑', '-')
+        text = self.__strip_boilerplate(text)
+        return text
+
+
+    def __is_heading_block(self, line, heading_traits) -> bool:
+        if line['font'] == heading_traits['font'] and line['size'] == heading_traits['size'] and line['color'] == heading_traits['color']:
+            return True
+        return False
